bdee583437b6d9ff6961b065ab78881c0c631a85
Matheus O <mmarqsoliveira@gmail.com>
2026-07-13 20:36:53 -0300

    refatorado main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,45 @@
-from core.lift import Lift
+import random
+
+from building.building import Building
+from cabin.cabin import Cabin
+from lift.controller import LiftController
+from lift.lift import Lift
+from lift.scheduler import LiftScheduler
+from passengers.passenger import PassengersGroupFactory
+from simulation.call_generator import CallGenerator
+from simulation.simulation import Simulation
+from utils.constants import TOTAL_FLOORS
 from utils.logger_config import config_logger
 
 
-def exec_lift() -> None:
+def build_simulation() -> Simulation:
+    building = Building(total_floors=TOTAL_FLOORS)
+    cabin = Cabin()
+    scheduler = LiftScheduler(building=building, cabin=cabin)
+    lift = Lift(building=building, cabin=cabin, scheduler=scheduler)
+    controller = LiftController(lift=lift)
+
+    passengers_factory = PassengersGroupFactory(
+        random_generator=random.Random()
+    )
+    call_generator = CallGenerator(
+        building=building,
+        passengers_factory=passengers_factory,
+        random_generator=random.Random(),
+        call_probability=0.05
+    )
+
+    return Simulation(
+        controller=controller,
+        call_generator=call_generator,
+    )
+
+
+def main() -> None:
     config_logger()
-    lift = Lift()
-    lift.operate_lift()
+    simulation = build_simulation()
+    while True:
+        simulation.run()
 
 
-exec_lift()
+main()
