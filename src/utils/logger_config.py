87061f97acddc47929e0b6101a232676156a8055
Matheus O <mmarqsoliveira@gmail.com>
2026-07-13 20:36:14 -0300

    configurado logger para gerar debugs no arquivo debug_logs.txt

--- src/utils/logger_config.py
+++ src/utils/logger_config.py
@@ -1,4 +1,11 @@
-from logging import DEBUG, basicConfig
+from logging import DEBUG, INFO, FileHandler, StreamHandler, basicConfig
+from sys import stdout
+
+console_handler = StreamHandler(stream=stdout)
+console_handler.setLevel(INFO)
+
+file_handler = FileHandler(filename="debug_logs.txt", mode="w")
+file_handler.setLevel(DEBUG)
 
 
 def config_logger() -> None:
@@ -7,4 +14,5 @@ def config_logger() -> None:
         encoding="utf-8",
         format="%(asctime)s %(levelname)s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[console_handler, file_handler]
     )
